3f2069a487280c54faf447e93e744b307cc9af78
YuriSolomon <juristein21@gmail.com>
2023-07-03 15:02:40 +0200

    create shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent {
-
+  ingredients: ingredient[] = [
+    new ingredient('Apples', 5),
+    new ingredient('Tomatoes', 10)
+  ];
 }
