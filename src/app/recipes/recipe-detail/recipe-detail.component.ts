f37a704f13bdd3c53dc347536358f3ee403a7dc2
YuriSolomon <juristein21@gmail.com>
2023-07-07 13:36:06 +0200

    add routing and change previous navigating and clicks behavious

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -11,10 +12,26 @@ import { RecipeService } from '../recipe.service';
 
 export class RecipeDetailComponent {
   @Input() recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) {};
+  constructor(private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router) {};
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
+      }
+    )
+  }
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 }
